2d60cd8b6aec7cb60dc8cc994b5b26d3cca43bc3
lilyLILYLL <lydatily@gmail.com>
2023-04-04 09:18:25 +1000

    Relocated App.js file

--- client/App.js
+++ client/App.js
@@ -1,7 +1,7 @@
 import "react-native-gesture-handler";
 
 import * as React from "react";
-import { Navigation } from "./navigations/Navigation";
+import { Navigation } from "./src/navigations/Navigation";
 export default function App() {
     return <Navigation />;
 }
